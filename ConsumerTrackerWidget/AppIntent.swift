516029b8d2899dff65e8c772a39ecbcda45549ae
Shah <shah.must.aqims@gmail.com>
2026-02-19 13:45:36 +0000

    Added the base layer for widgets

---
+++ ConsumerTrackerWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  ConsumerTrackerWidget
+//
+//  Created by Shahril S M B (FCES) on 19/02/2026.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ ConsumerTrackerWidget/ConsumerTrackerWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumerTrackerWidgetBundle.swift
+//  ConsumerTrackerWidget
+//
+//  Created by Shahril S M B (FCES) on 19/02/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ConsumerTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ConsumerTrackerWidget()
+        ConsumerTrackerWidgetControl()
+    }
+}
